a097b36eef6ddd2b1161429f2992f3b3c2a03c67
Dheeraj B N <dheeraj.bn@valtech.com>
2023-03-24 18:01:17 +0530

    mail templtes

---
+++ SecureSeatBooking/src/main/java/com/SecureSeat/Booking/service/MailTemplates.java
@@ -0,0 +1,9 @@
+package com.SecureSeat.Booking.service;
+
+import com.SecureSeat.Booking.entity.UserDeatils;
+
+public interface MailTemplates {
+
+	String registrationMail(UserDeatils userInfo);
+
+}
