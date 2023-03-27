930015c13efaad262f464eaed256b3a9f6487c0e
Divyashree M D <divyashree.md@valtech.com>
2023-03-27 17:36:21 +0530

    logic for seat booking and logic to check seat booked or not

--- SecureSeatBooking/src/main/java/com/SecureSeat/Booking/service/SeatBookImpl.java
+++ SecureSeatBooking/src/main/java/com/SecureSeat/Booking/service/SeatBookImpl.java
@@ -24,17 +24,28 @@ public class SeatBookImpl implements SeatBook {
 
 	@Autowired
 	private HolidayDetailsRepo holidayDetailsRepo;
+	
+	@Override
+	public String savebookeddetails(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
+		String message;
+		if(from.equals(to)) {
+			 message = saveBookedDetailsforday(bookingDetails, from, to);
+					return message;
+		}
+		else {
+		 message = seatbookingforweek(bookingDetails, from, to);
+					return message;
+			
+		}
+	}
 
 	@Override
-	public String saveBookedDetails(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
+	public String saveBookedDetailsforday(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
 		int flag = 0;
-		int checkseat=checkseatalreadybooked(bookingDetails);
-		
-		if (checkseat == 0) {
-				int chechholiday = checkholiday(from);				
+				int chechholiday = checkholiday(from);	
+				System.out.println(chechholiday);
 				if (chechholiday ==0) {
-					int checkuser = checkbookingdetails(from,bookingDetails.getUserDeatils());
-					System.out.println(checkuser);
+					int checkuser = checkbookingdetails(from,bookingDetails);
 					if (checkuser ==0) {
 						LocalDate d1 = from;
 						bookingDetails.setDate(from);
@@ -44,7 +55,7 @@ public class SeatBookImpl implements SeatBook {
 						UserDeatils user = bookingDetails.getUserDeatils();
 						Employee employee = user.getEmployee();
 						int employeeid = employee.getEmployeeId();
-						String tokenvalue = date1 + employeeid;
+						String tokenvalue = date1 +employeeid;
 						bookingDetails.setToken(tokenvalue);
 						bookingDetails.setBookingStatus("PENDING");
 						BookingDetails b = bookingDetailsRepo.save(bookingDetails);
@@ -63,20 +74,16 @@ public class SeatBookImpl implements SeatBook {
             
 			}
 		
-		else {
-			return "That seat has been already booked for that date";
-		}
-	}
+	
+
   @Override
 	public String seatbookingforweek(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
-	  int checkseat=checkseatalreadybooked(bookingDetails);
 	  
 			LocalDate to1 = to.plusDays(1);
 			for (LocalDate i = from; i.isBefore(to1); i = i.plusDays(1)) {
 				int checkholiday = checkholiday(i);
 				if (checkholiday==0) {
-					int checkuser = checkbookingdetails(i,bookingDetails.getUserDeatils());
-					System.out.println(checkuser);
+					int checkuser = checkbookingdetails(i,bookingDetails);
 					if(checkuser==0) {
 					LocalDate d1 = i;
 					bookingDetails.setDate(i);
@@ -96,17 +103,22 @@ public class SeatBookImpl implements SeatBook {
 				}
 
 			}
+				
 			}
 			
 			return "seat has been booked for week";
 
 		
 	}
-	
+	@Override
 	public int checkholiday(LocalDate date) {
 		int flag =0;
-		try {
+		
 			List<HolidayDetails> holidayDetails = holidayDetailsRepo.findAll();
+			if(holidayDetails.isEmpty()) {
+				return 0;
+			}
+			else {
 			for (HolidayDetails holi : holidayDetails) {
 
 				if ((date.equals(holi.getHolidayDate()))) {
@@ -119,59 +131,54 @@ public class SeatBookImpl implements SeatBook {
 			else {
 				return 1;
 			}
-		}
-		catch (Exception e) {
-			return 0;
-		}
+			}
+		
 	}
-	
-	public int checkbookingdetails(LocalDate date,UserDeatils userdetDeatils) {
+	@Override
+	public int checkbookingdetails(LocalDate date,BookingDetails bookingDetails1) {
 		int flag=0;
-		try {
-			List<BookingDetails> bookingDetails = bookingDetailsRepo.findAllByUserDeatils(userdetDeatils);
+		
+			List<BookingDetails> bookingDetails = bookingDetailsRepo.findAllByUserDeatils(bookingDetails1.getUserDeatils());
 			System.out.println(bookingDetails);
 			for (BookingDetails book2 : bookingDetails) {
-            
-				if (date.equals(book2.getDate())){
-					if(userdetDeatils.equals(book2.getUserDeatils())) {
-					flag = 1;
-				}
+				if ((date.equals(book2.getDate())) &&
+					(bookingDetails1.getUserDeatils().getUserId()==book2.getUserDeatils().getUserId())) {
+					flag= 1;
 					
 				}
 			}
-			if(flag==0) {
+			if(flag==1 ) {
 				return 1;
 			}
 			else {
 				return 0;
 			}
 			
-		}
-		catch (Exception e) {
-			return 0;
-		}
+			
+			
+		
 	}
 	
-	public int checkseatalreadybooked(BookingDetails bookingDetails) {
+	@Override
+	public String checkseatalreadybooked(String seatNo,LocalDate date1) {
 		int flag=0;
-		try {
 			List<BookingDetails> bookDetails = bookingDetailsRepo.findAll();
 			for (BookingDetails book: bookDetails) {
-				if(bookingDetails.getSeatNo().equals(book.getSeatNo()) && bookingDetails.getDate().equals(book.getDate())) {
+				if(seatNo.equals(book.getSeatNo()) && date1.equals(book.getDate())) {
 					flag = 1;
 				}
 			}
-			if(flag==0) {
-				return 0;
+			if(flag==1) {
+				return "seat Booked";
 			}
 			else {
-				return 1;
+				return "seat not booked";
 			}
 			
-		}catch (Exception e) {
-			return 0;
-		}
+		
 		
 	}
+	
+	
 
 }
