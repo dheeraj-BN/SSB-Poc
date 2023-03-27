bfb074919e2b00ccc2487ea1d9ebc9f451890c3b
Divyashree M D <divyashree.md@valtech.com>
2023-03-27 14:22:48 +0530

    checking user already booked seat for that particular day

--- SecureSeatBooking/src/main/java/com/SecureSeat/Booking/service/SeatBookImpl.java
+++ SecureSeatBooking/src/main/java/com/SecureSeat/Booking/service/SeatBookImpl.java
@@ -27,49 +27,15 @@ public class SeatBookImpl implements SeatBook {
 
 	@Override
 	public String saveBookedDetails(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
-       
-		String seatNo = bookingDetails.getSeatNo();
-		System.out.println(from.equals(to));
-		if (from.equals(to)) {
-			int flag3 = 0;
-			int flag6 = 0;
-			List<HolidayDetails> holiday = holidayDetailsRepo.findAll();
-			for (HolidayDetails holi : holiday) {
-
-				if (!(from.equals(holi.getHolidayDate()))) {
-					flag3 = 1;
-				} else {
-					flag6 = 1;
-				}
-			}
-			if (flag3 == 1 && flag6 == 0) {
-				List<BookingDetails> userdetails = bookingDetailsRepo
-						.findByUserDeatils(bookingDetails.getUserDeatils());
-				if (userdetails.isEmpty()) {
-					LocalDate d1 = from;
-					bookingDetails.setDate(from);
-					bookingDetails.setBookedTimings(LocalTime.now());
-					String date1 = d1.format(DateTimeFormatter.ofPattern("ddMMYYYY"));
-					System.out.println(date1);
-					UserDeatils user = bookingDetails.getUserDeatils();
-					Employee employee = user.getEmployee();
-					int employeeid = employee.getEmployeeId();
-					String tokenvalue = date1 + employeeid;
-					bookingDetails.setToken(tokenvalue);
-					bookingDetails.setBookingStatus("PENDING");
-					BookingDetails b = bookingDetailsRepo.save(bookingDetails);
-
-				} else {
-					int flag1 = 0;
-					for (BookingDetails book2 : userdetails) {
-
-						if (!(!(from.equals(book2.getDate()))
-								&& !(bookingDetails.getUserDeatils().equals(book2.getUserDeatils())))) {
-							flag1 = 1;
-						}
-					}
-
-					if (flag1 == 0) {
+		int flag = 0;
+		int checkseat=checkseatalreadybooked(bookingDetails);
+		
+		if (checkseat == 0) {
+				int chechholiday = checkholiday(from);				
+				if (chechholiday ==0) {
+					int checkuser = checkbookingdetails(from,bookingDetails.getUserDeatils());
+					System.out.println(checkuser);
+					if (checkuser ==0) {
 						LocalDate d1 = from;
 						bookingDetails.setDate(from);
 						bookingDetails.setBookedTimings(LocalTime.now());
@@ -82,93 +48,130 @@ public class SeatBookImpl implements SeatBook {
 						bookingDetails.setToken(tokenvalue);
 						bookingDetails.setBookingStatus("PENDING");
 						BookingDetails b = bookingDetailsRepo.save(bookingDetails);
+						return "Seat has been booked for today";
 
-					}
+					} else {
+						
+							return "Seat has already  booked for today";
+						}
 				}
-
+						
+					
+				else {
+					return "That is holiday you can't able to book";
+				}
+            
 			}
-			return "Seat Booking has been done for today";
-		}
-
+		
 		else {
-			List<HolidayDetails> holiday = holidayDetailsRepo.findAll();
-			List<BookingDetails> userdetails = bookingDetailsRepo.findByUserDeatils(bookingDetails.getUserDeatils());
-			if (userdetails.isEmpty()) {
-                  LocalDate to1= to.plusDays(1);
-				for (LocalDate i = from; i.isBefore(to1); i = i.plusDays(1)) {
-					int flag4 = 0;
-					int flag5 = 0;
-					for (HolidayDetails holi : holiday) {
-						if (!(i.equals(holi.getHolidayDate()))) {
-							flag4 = 1;
-						} else {
-							flag5 = 1;
-						}
+			return "That seat has been already booked for that date";
+		}
+	}
+  @Override
+	public String seatbookingforweek(BookingDetails bookingDetails, LocalDate from, LocalDate to) {
+	  int checkseat=checkseatalreadybooked(bookingDetails);
+	  
+			LocalDate to1 = to.plusDays(1);
+			for (LocalDate i = from; i.isBefore(to1); i = i.plusDays(1)) {
+				int checkholiday = checkholiday(i);
+				if (checkholiday==0) {
+					int checkuser = checkbookingdetails(i,bookingDetails.getUserDeatils());
+					System.out.println(checkuser);
+					if(checkuser==0) {
+					LocalDate d1 = i;
+					bookingDetails.setDate(i);
+					bookingDetails.setBookedTimings(LocalTime.now());
+					String date1 = d1.format(DateTimeFormatter.ofPattern("ddMMYYYY"));
+					System.out.println(date1);
+					UserDeatils user = bookingDetails.getUserDeatils();
+					Employee employee = user.getEmployee();
+					int employeeid = employee.getEmployeeId();
+					String tokenvalue = date1 + employeeid;
+					bookingDetails.setToken(tokenvalue);
 
-					}
-					if (flag4 == 1 && flag5 == 0) {
-						LocalDate d1 = i;
-						bookingDetails.setDate(i);
-						bookingDetails.setBookedTimings(LocalTime.now());
-						String date1 = d1.format(DateTimeFormatter.ofPattern("ddMMYYYY"));
-						System.out.println(date1);
-						UserDeatils user = bookingDetails.getUserDeatils();
-						Employee employee = user.getEmployee();
-						int employeeid = employee.getEmployeeId();
-						String tokenvalue = date1 + employeeid;
-						bookingDetails.setToken(tokenvalue);
-
-						BookingDetails book = new BookingDetails(bookingDetails.getSeatNo(),
-								bookingDetails.isFoodStatus(), d1, LocalTime.now(), null, "PENDING", tokenvalue, user,
-								bookingDetails.getShiftDetails());
-						BookingDetails b = bookingDetailsRepo.save(book);
-
-					}
+					BookingDetails book = new BookingDetails(bookingDetails.getSeatNo(), bookingDetails.isFoodStatus(),
+							d1, LocalTime.now(), null, "PENDING", tokenvalue, user, bookingDetails.getShiftDetails());
+					BookingDetails b = bookingDetailsRepo.save(book);
 
 				}
-			} else {
-				LocalDate to1 = to.plusDays(1);
-				for (LocalDate i = from; i.isBefore(to1); i = i.plusDays(1)) {
-					int flag = 0;
-					for (HolidayDetails holi : holiday) {
-						System.out.println(!(i.equals(holi.getHolidayDate())));
-						if (!(i.equals(holi.getHolidayDate()))) {
-							for (BookingDetails book2 : userdetails) {
-								if (!(!(i.equals(book2.getDate()))
-										&& !(bookingDetails.getUserDeatils().equals(book2.getUserDeatils())))) {
-									flag = 1;
-								}
 
-							}
-						} else {
-							flag = 1;
-						}
+			}
+			}
+			
+			return "seat has been booked for week";
 
-					}
+		
+	}
+	
+	public int checkholiday(LocalDate date) {
+		int flag =0;
+		try {
+			List<HolidayDetails> holidayDetails = holidayDetailsRepo.findAll();
+			for (HolidayDetails holi : holidayDetails) {
 
-					if (flag == 0) {
-						LocalDate d1 = i;
-						bookingDetails.setDate(i);
-						bookingDetails.setBookedTimings(LocalTime.now());
-						String date1 = d1.format(DateTimeFormatter.ofPattern("ddMMYYYY"));
-						System.out.println(date1);
-						UserDeatils user = bookingDetails.getUserDeatils();
-						Employee employee = user.getEmployee();
-						int employeeid = employee.getEmployeeId();
-						String tokenvalue = date1 + employeeid;
-						bookingDetails.setToken(tokenvalue);
-
-						BookingDetails book = new BookingDetails(bookingDetails.getSeatNo(),
-								bookingDetails.isFoodStatus(), d1, LocalTime.now(), null, "PENDING", tokenvalue, user,
-								bookingDetails.getShiftDetails());
-						BookingDetails b = bookingDetailsRepo.save(book);
-					}
-
+				if ((date.equals(holi.getHolidayDate()))) {
+					flag = 1;
+				} 
+			}
+			if(flag==0) {
+				return 0;
+			}
+			else {
+				return 1;
+			}
+		}
+		catch (Exception e) {
+			return 0;
+		}
+	}
+	
+	public int checkbookingdetails(LocalDate date,UserDeatils userdetDeatils) {
+		int flag=0;
+		try {
+			List<BookingDetails> bookingDetails = bookingDetailsRepo.findAllByUserDeatils(userdetDeatils);
+			System.out.println(bookingDetails);
+			for (BookingDetails book2 : bookingDetails) {
+            
+				if (date.equals(book2.getDate())){
+					if(userdetDeatils.equals(book2.getUserDeatils())) {
+					flag = 1;
+				}
+					
 				}
 			}
-			return "Seat Booking has been done for week ";
+			if(flag==0) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+			
 		}
-
+		catch (Exception e) {
+			return 0;
+		}
+	}
+	
+	public int checkseatalreadybooked(BookingDetails bookingDetails) {
+		int flag=0;
+		try {
+			List<BookingDetails> bookDetails = bookingDetailsRepo.findAll();
+			for (BookingDetails book: bookDetails) {
+				if(bookingDetails.getSeatNo().equals(book.getSeatNo()) && bookingDetails.getDate().equals(book.getDate())) {
+					flag = 1;
+				}
+			}
+			if(flag==0) {
+				return 0;
+			}
+			else {
+				return 1;
+			}
+			
+		}catch (Exception e) {
+			return 0;
+		}
+		
 	}
 
 }
