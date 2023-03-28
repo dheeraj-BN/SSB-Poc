e1f9114fcb5136afdd3ed127093733550698716e
akshay kk <akshay.kk@valtech.com>
2023-03-28 10:58:54 +0530

    validate token JDBC Template Query using JOINS Working

--- SecureSeatBooking/src/main/java/com/SecureSeat/Booking/dao/UserDetailDao.java
+++ SecureSeatBooking/src/main/java/com/SecureSeat/Booking/dao/UserDetailDao.java
@@ -5,8 +5,11 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.SecureSeat.Booking.entity.Employee;
 
 @Component
 public class UserDetailDao {
@@ -18,6 +21,27 @@ public class UserDetailDao {
 		
 		String sql = "UPDATE `seatsb`.`booking_details` SET `login_time` = ?, `booking_status` = ? WHERE (`booking_id` = ?)";
 	    jdbcTemplate.update(sql, loggedIn, status, id);
+	}
+	
+	
+	public Employee getEmployeeDetailsAfterValidationSuccess(String token) {
+		
+		String sql = "SELECT e.* FROM booking_details bd " +
+                "INNER JOIN user_deatils ud ON bd.user_id = ud.user_id " +
+                "INNER JOIN employee e ON ud.employee_id = e.employee_id " +
+                "WHERE bd.token = ?";
+		
+		
+		 return jdbcTemplate.queryForObject(sql, new Object[]{token}, (rs, rowNum) ->
+         new Employee(
+                 rs.getInt("employee_id"),
+                 rs.getString("employee_name"),
+                 rs.getString("employee_email"),
+                 rs.getString("employee_designation"),
+                 rs.getString("employee_phone_no"),
+                 rs.getString("employee_gender"),
+                 rs.getString("employee_personal_email")
+         ));
 	}
 
 }
