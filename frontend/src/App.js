491a793cf9cd399e5a7c2b4a8049e51fe3e93baf
Mihir Kumar <mihir.kumar@valtech.com>
2023-03-28 10:51:03 +0530

    User dashboard and seatbooking React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
    {/* <UserDashBoard/> */}
      {/* <SeatBookingForm/> */}
      {/* <FloorList/> */}
-    < UserDashboard/>
+    {/* < UserDashboard/> */}
     <Router>
       <Routes>
         <Route path="/"  element={<UserDashBoard/>} />
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -3,16 +3,16 @@ import { useNavigate } from "react-router-dom";
 import "../components/UserDashboard.css"
 
 function UserDashboard() {
-  const navigate=useNavigate()
-  function SeatBookingForm(){
-    navigate("/booking")
-  }
+  // const navigate=useNavigate()
+  // function SeatBookingForm(){
+  //   navigate("/booking")
+  // }
   return (
    
       <div>
       <nav>
         <ul>
-          <li><button onClick={SeatBookingForm}>Seat Booking</button></li>
+          <li><button><a href="/booking">Seat Booking</a></button></li>
           <li><button>View Profile</button></li>
           <li><button><i class="fas fa-bell"></i></button></li>
         </ul>
