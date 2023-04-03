418f9b64b4e5798b904a6961be5a9e0c5e3ce234
Archies Singh <archies.singh@valtech.com>
2023-04-03 18:20:47 +0530

    resolve code conflict

--- frontend Archies/src/components/UserDashboard/UserDash.jsx
+++ frontend Archies/src/components/UserDashboard/UserDash.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import "../../css/userDashboard/navbar.css";
 import "../../css/userDashboard/UserDashboard.css";
-<<<<<<< HEAD
-import NavBar from "./NavBar";
-import QRGenerator from "./qrgenerator";
-=======
->>>>>>> 858ad4605e961299982bdf4d5f7f523bbd598360
 
 function UserDashboard() {
   return (
     <div>
-<<<<<<< HEAD
-      <div>
-        <NavBar />
-      </div>
-      <h1>Welcome to your dashboard Mihir</h1>
-      {/* <a href="/qr">
-        <button className="btn btn-primary">View Booked Seat</button>
-        </a> */}
-      <div className="qr">
-        <QRGenerator />
-      </div>
-=======
       <nav className="nav">
         <ul>
           <li>
@@ -47,7 +29,6 @@ function UserDashboard() {
         </ul>
       </nav>
       <h1>Welcome to your dashboard</h1>
->>>>>>> 858ad4605e961299982bdf4d5f7f523bbd598360
     </div>
   );
 }
