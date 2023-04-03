4bb8960689269931e2bb27631e4231d92e47c7df
mihir952 <66687694+mihir952@users.noreply.github.com>
2023-04-03 18:39:32 +0530

    Update UserDash.jsx

--- frontend Archies/src/components/UserDashboard/UserDash.jsx
+++ frontend Archies/src/components/UserDashboard/UserDash.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../css/userDashboard/navbar.css";
-import "../../css/userDashboard/UserDashboard.css";
+import "../../css/userDashboard/navbar.css";
 import NavBar from "./NavBar";
 import QRGenerator from "./qrgenerator";
+
 function UserDashboard() {
   return (
     <div>
-
       <div>
         <NavBar />
       </div>
@@ -17,33 +17,9 @@ function UserDashboard() {
       <div className="qr">
         <QRGenerator />
       </div>
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="/booking">
-              <button>Seat Booking</button>
-            </a>
-          </li>
-          <li>  
-            <a href="/profileuser">
-              <button>View Profile</button>
-            </a>
-          </li>
-          <li>
-            <a href="/qr">
-              <button>View Seat Booking</button>
-            </a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <i class="bi bi-box-arrow-right">Sign Out</i>
-          </li>
-        </ul>
-      </nav>
-      <h1>Welcome to your dashboard</h1>
     </div>
   );
 }
 
 export default UserDashboard;
+
