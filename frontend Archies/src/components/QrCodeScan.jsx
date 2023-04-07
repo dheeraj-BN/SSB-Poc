35b9fca25736229899de4923de5a5183a3171a24
Archies Singh <archies.singh@valtech.com>
2023-04-07 12:29:42 +0530

    completed qrscanning

--- frontend Archies/src/components/QrCodeScan.jsx
+++ frontend Archies/src/components/QrCodeScan.jsx
@@ -3,11 +3,41 @@ import { QrReader } from "react-qr-reader";
 import { useState } from "react";
 
 function QrCodeScan(props) {
-  const [webcamResult, setwebcamResult] = useState();
+  const [webcamResult, setwebcamResult] = useState('');
+  const [qrdata, setqrdata] = useState([])
+  const [token, setToken] = useState(window.localStorage.getItem("token"))
   useEffect(()=>{
     props.data(webcamResult)
+    fetch(`http://10.191.80.98:9090/api/admin/validateToken/?token=${webcamResult}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        } 
+        return response.json();
+      })
+      .then((data) => {
+        // setShifts(newShifts);
+        // console.log(data)
+        setqrdata(data)
+        
+      })
+      .catch((error) => {
+        console.error("There was an error deleting the shift:", error);
+      });
   },[webcamResult])
 
+  useEffect(()=>{
+    props.emp(qrdata)
+  },[qrdata])
+
+
+
+  
 
   return (
     <div className="card col-sm-3 m-2">
@@ -33,8 +63,10 @@ function QrCodeScan(props) {
         {/* <div className="card-footer rounded mb-1">
           <h6>Webcam Result : {webcamResult} </h6>
         </div> */}
+       
     </div>
   );
 }
 
 export default QrCodeScan;
+
